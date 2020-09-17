4cb70cb9ac70475d8ba143052d7440153611680a
kittisak <doomusrimyy@gmail.com>
2020-09-17 10:32:31 +0700

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './modals/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
