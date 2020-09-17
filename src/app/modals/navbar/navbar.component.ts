0401fc95482b2fd7dfed8a096787e9009bd5d531
kittisak <doomusrimyy@gmail.com>
2020-09-17 10:10:28 +0700

    test

--- src/app/modals/navbar/navbar.component.ts
+++ src/app/modals/navbar/navbar.component.ts
@@ -11,5 +11,12 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  onClickOpenMenu(){
+    document.getElementById("leftMenu").style.display = "block";
+    document.getElementById("leftMenu").style.width = "300px";
+  }
+  onClickCloseMenu(){
+    document.getElementById("leftMenu").style.display = "none";
+    
+  }
 }
