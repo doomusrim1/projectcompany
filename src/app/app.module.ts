556dde80b4ddc74695478552224e90dfc084585c
unknown <boom.bbb.boom@gmail.com>
2020-09-17 15:50:31 +0700

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { AdminComponent } from './modals/navbar/admin/admin.component';
 import { UserComponent } from './modals/navbar/user/user.component';
 import { MoneyComponent } from './pages/user/money/money.component';
+import { AdminMonneyComponent } from './pages/admin/admin-monney/admin-monney.component';
 
 
 @NgModule({
@@ -17,6 +18,7 @@ import { MoneyComponent } from './pages/user/money/money.component';
     AdminComponent,
     UserComponent,
     MoneyComponent,
+    AdminMonneyComponent,
   ],
   imports: [
     BrowserModule,
