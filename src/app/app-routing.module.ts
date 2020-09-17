9a504a029224ebea46113b6a06ef287dc2fcf4c7
kittisak <doomusrimyy@gmail.com>
2020-09-17 11:52:18 +0700

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { MoneyComponent } from './pages/user/money/money.component';
 
 const routes: Routes = [
   {path: '',component: HomeComponent},
+  {path: 'user/money',component: MoneyComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './pages/home/home.component';
 import { AdminComponent } from './modals/navbar/admin/admin.component';
 import { UserComponent } from './modals/navbar/user/user.component';
+import { MoneyComponent } from './pages/user/money/money.component';
 
 
 @NgModule({
@@ -15,6 +16,7 @@ import { UserComponent } from './modals/navbar/user/user.component';
     HomeComponent,
     AdminComponent,
     UserComponent,
+    MoneyComponent,
   ],
   imports: [
     BrowserModule,
