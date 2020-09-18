db839e522b7ca235416268ba841f920edbcb1c30
kittisak <doomusrimyy@gmail.com>
2020-09-18 16:18:25 +0700

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { AdminMonneyComponent } from './pages/admin/admin-monney/admin-monney.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MoneyComponent } from './pages/user/money/money.component';
+import { ItemComponent } from './pages/user/productitem/item/item.component';
 
 const routes: Routes = [
   {path: '',component: HomeComponent},
   {path: 'user/money',component: MoneyComponent},
+  {path: 'productitem/item',component:ItemComponent},
 
   {path: 'admin/admin-money',component: AdminMonneyComponent},
 
