2fe862439b18312ea2d501b7f8984fd6f5edeca4
kittisak <doomusrimyy@gmail.com>
2020-09-23 14:23:52 +0700

    test

--- src/app/pages/user/sale/sale-addmoney/sale-addmoney.component.ts
+++ src/app/pages/user/sale/sale-addmoney/sale-addmoney.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sale-addmoney.component.scss']
 })
 export class SaleAddmoneyComponent implements OnInit {
-
+  round:string = "อาหาร"
+  code:number = 54451445;
+  name:string = "สมใจ มีสุข";
   constructor() { }
 
   ngOnInit(): void {
--- src/app/pages/user/sale/sale-money/sale-money.component.ts
+++ src/app/pages/user/sale/sale-money/sale-money.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sale-money.component.scss']
 })
 export class SaleMoneyComponent implements OnInit {
-
+  code:string = "54451445";
+  name:string = "สมใจ มีสุข";
   constructor() { }
 
   ngOnInit(): void {
