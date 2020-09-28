25cc53db97e59e1e7780221ce9525db0f542e88f
unknown <boom.bbb.boom@gmail.com>
2020-09-28 13:33:17 +0700

    new boom naja

--- src/app/pages/admin/admin-item/admin-item-get/admin-item-get.component.ts
+++ src/app/pages/admin/admin-item/admin-item-get/admin-item-get.component.ts
@@ -7,7 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminItemGetComponent implements OnInit {
 
-  constructor() { }
+  config: any;
+  collection = { count: 25, data: [] };
+
+  constructor() { 
+
+    for (var i = 0; i < this.collection.count; i++) {
+      this.collection.data.push(
+        {
+          id: i + 101,
+          value: "... " + (i + 1)
+        }
+      );
+    }
+
+    this.config = {
+      itemsPerPage: 5,
+      currentPage: 1,
+      totalItems: this.collection.count
+    };
+
+  }
+  pageChanged(event){
+    this.config.currentPage = event;
+  }
 
   ngOnInit(): void {
   }
