ac048c7f47b650d081992505353c961a95e7e19b
unknown <boom.bbb.boom@gmail.com>
2020-09-30 10:50:00 +0700

    new naja

--- src/app/modals/navbar/admin/menu-ber/menu-ber-monney/menu-ber-monney.component.ts
+++ src/app/modals/navbar/admin/menu-ber/menu-ber-monney/menu-ber-monney.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-ber-monney.component.scss']
 })
 export class MenuBerMonneyComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit(): void {
